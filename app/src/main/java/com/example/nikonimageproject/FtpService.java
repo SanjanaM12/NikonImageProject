7a5b806448caae541c5936f660149dbf96748c99
SanjanaM12 <2sanjanam@gmail.com>
2026-08-27 15:19:07 -0700

    Set up connection and lifecycle of server

--- app/src/main/java/com/example/nikonimageproject/FtpService.java
+++ app/src/main/java/com/example/nikonimageproject/FtpService.java
@@ -3,11 +3,19 @@ package com.example.nikonimageproject;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import java.io.File;
 
-public class FtpService extends Service {
+public class FtpService extends Service implements CameraFtpServer.OnPhotoReceivedListener{
+    private static final String TAG = "FTPService";
     private CameraFtpServer ftpServer;
+    private NetworkManager networkManager;
+
+    @Override
+    public void onCreate(){
+        networkManager = new NetworkManager(this);
+    }
 
     @Override
     //allows UI to call function inside background process directly
@@ -18,21 +26,32 @@ public class FtpService extends Service {
     @Override
     //entry point whenever a startService is called
     public int onStartCommand(Intent intent, int flags, int startID) {
+        //Pin socket routing to camera Wifi hotspot
+        if (networkManager != null){
+            networkManager.bindToWifi();
+        }
+
         //creates the path for the incoming images
         File incomingDir = new File(getExternalFilesDir(null), "incoming");
         if (!incomingDir.exists()) {
-            incomingDir.mkdir();
+            incomingDir.mkdirs();
         }
 
         //creates a new Server and starts it
-        ftpServer = new CameraFtpServer((CameraFtpServer.OnPhotoReceivedListener) this);
+        ftpServer = new CameraFtpServer(this);
         try {
             ftpServer.start(incomingDir, 2221);
+            Log.i(TAG, "FTP Server started on port 2221");
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.e(TAG, "Failed to start FTP Server: " + e.getMessage(), e);
         }
 
         return START_STICKY; //allows for continuous background server
+    }
+
+    @Override
+    public void onPhotoReceived(File file) {
+        Log.i(TAG, "New photo successfully received: " + file.getAbsolutePath());
     }
 
     @Override
@@ -42,5 +61,9 @@ public class FtpService extends Service {
         if (ftpServer != null){
             ftpServer.stop();
         } //stops server if it was running
+        if (networkManager != null){
+            networkManager.unbindFromWifi();
+        }
+
     }
 }

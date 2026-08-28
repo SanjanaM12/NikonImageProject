625f93e1502d49715d077f12d7ef43b88b59fdcf
SanjanaM12 <2sanjanam@gmail.com>
2026-08-28 12:43:45 -0700

    Implemented server start connection method

--- app/src/main/java/com/example/nikonimageproject/NikonPtpServer.java
+++ app/src/main/java/com/example/nikonimageproject/NikonPtpServer.java
@@ -1,11 +1,19 @@
 package com.example.nikonimageproject;
 
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.util.UUID;
 
 public class NikonPtpServer {
     private static final String TAG = "NikonPtpServer";
     public static final int PTP_PORT = 15470;
+
+    private ServerSocket serverSocket;
+    private boolean isRunning = false;
 
     private final byte[] hostGuid = new byte[16];
 
@@ -15,5 +23,27 @@ public class NikonPtpServer {
         UUID uuid = UUID.randomUUID();
         bb.putLong(uuid.getMostSignificantBits());
         bb.putLong(uuid.getLeastSignificantBits());
+    }
+
+    public void start() {
+        isRunning = true;
+        //background thread
+        new Thread(() -> {
+            try {
+                //directs network traffic to this port
+                serverSocket = new ServerSocket(PTP_PORT);
+                Log.i(TAG, "PTP/IP Server listening on port " + PTP_PORT);
+
+                //waits until client initiates a TCP connection
+                while (isRunning){
+                    Socket socket = serverSocket.accept();
+                    Log.i(TAG, "Incoming connection from camera: " + socket.getInetAddress());
+                }
+            } catch (IOException e) {
+                if (isRunning){
+                    Log.e(TAG, "Server error: " + e.getMessage());
+                }
+            }
+        }).start(); //creates worker thread to manage specific client socket
     }
 }

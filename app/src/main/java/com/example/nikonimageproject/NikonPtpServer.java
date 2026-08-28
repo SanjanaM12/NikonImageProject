4b5dc1248c73df6c166a45ca628ef614b21f4c79
SanjanaM12 <2sanjanam@gmail.com>
2026-08-28 12:10:53 -0700

    Implemented constructor for ptp server

--- app/src/main/java/com/example/nikonimageproject/NikonPtpServer.java
+++ app/src/main/java/com/example/nikonimageproject/NikonPtpServer.java
@@ -1,4 +1,19 @@
 package com.example.nikonimageproject;
 
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
 public class NikonPtpServer {
+    private static final String TAG = "NikonPtpServer";
+    public static final int PTP_PORT = 15470;
+
+    private final byte[] hostGuid = new byte[16];
+
+    //Constructor that creates unique id for the Nikon GUID
+    public NikonPtpServer(){
+        ByteBuffer bb = ByteBuffer.wrap(hostGuid);
+        UUID uuid = UUID.randomUUID();
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+    }
 }

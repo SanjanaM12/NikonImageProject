cef71c0ca279e74ff1bba4a87ba3f92d847c86c4
SanjanaM12 <2sanjanam@gmail.com>
2026-09-03 21:19:18 -0700

    Fixed display photo

--- app/src/main/java/com/example/nikonimageproject/MainActivity.java
+++ app/src/main/java/com/example/nikonimageproject/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.nikonimageproject;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Collections;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView ivLatestPhoto;
+
+    private final BroadcastReceiver photoReceiver = new BroadcastReceiver() {
+        //Catch event and display photo
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String imagePath = intent.getStringExtra("image_path");
+            if(imagePath != null){
+                //Decode file and display on UI
+                Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+                ivLatestPhoto.setImageBitmap(bitmap);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,40 +36,8 @@ public class MainActivity extends AppCompatActivity {
         //Links Java variables to their XML layout counterparts by ID
         ivLatestPhoto = findViewById(R.id.ivLatestPhoto);
 
+        //Start background service
         Intent serviceIntent = new Intent(this, FtpService.class);
         startForegroundService(serviceIntent);
-    }
-
-    private String getDeviceIpAddress() {
-        try {
-            //get all network interfaces
-            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface intf : interfaces) {
-                //get all ip addresses of this interface
-                List<InetAddress> addresses = Collections.list(intf.getInetAddresses());
-                for (InetAddress addr : addresses) {
-                    //filter out loopback (home) and IPv6 addresses (select only Ipv4)
-                    if (!addr.isLoopbackAddress() && addr instanceof Inet4Address) {
-                        String host = addr.getHostAddress();
-                        if (host.startsWith("192.168.43.") || host.startsWith("192.168.49.")) {
-                            return host;
-                        }
-                    }
-
-                }
-            }
-
-            for (NetworkInterface intf : interfaces) {
-                List<InetAddress> addresses = Collections.list(intf.getInetAddresses());
-                for (InetAddress addr : addresses) {
-                    if (!addr.isLoopbackAddress() && addr instanceof Inet4Address) {
-                        return addr.getHostAddress();
-                    }
-                }
-            }
-        } catch (Exception ignored) {
-        }
-        //Defauly if no active wifi interface found
-        return "127.0.0.1";
     }
 }

3465f2b98adb55dd484dd0861b2d4d144a1a7a03
SanjanaM12 <2sanjanam@gmail.com>
2026-09-03 21:15:27 -0700

    Cleaned up on create method

--- app/src/main/java/com/example/nikonimageproject/MainActivity.java
+++ app/src/main/java/com/example/nikonimageproject/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.nikonimageproject;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.net.Inet4Address;
 import java.net.InetAddress;
@@ -18,42 +14,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvIpAddress;
-    private Button btnStartServer;
-    private Button btnStopServer;
+    private ImageView ivLatestPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         //Links Java variables to their XML layout counterparts by ID
-        tvIpAddress = findViewById(R.id.tvIpAddress);
-        btnStartServer = findViewById(R.id.btnStartServer);
-        btnStopServer = findViewById(R.id.btnStopServer);
+        ivLatestPhoto = findViewById(R.id.ivLatestPhoto);
 
-        //Applies system bar insets as padding
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        btnStartServer.setOnClickListener(v -> {
-            Intent startIntent = new Intent(this, FtpService.class);
-            startService(startIntent);
-
-            String ip = getDeviceIpAddress();
-            tvIpAddress.setText("Server Running\nIP:" + ip + "\nPort: 15740");
-        });
-
-        btnStopServer.setOnClickListener(v -> {
-            //Creates Intent targeting FTPService to trigger onDestroy and stop server
-            Intent stopIntent = new Intent(this, FtpService.class);
-            stopService(stopIntent);
-            tvIpAddress.setText("Server: Stopped");
-        });
+        Intent serviceIntent = new Intent(this, FtpService.class);
+        startForegroundService(serviceIntent);
     }
 
     private String getDeviceIpAddress() {
